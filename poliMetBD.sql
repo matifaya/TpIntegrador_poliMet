5848d9b636507c76f666db44a9a83e8777c20afd
joacogonz32 <alumno26.gonzalez.parodi.joaquin@ipm.edu.ar>
2025-10-15 08:43:11 -0300

    Update poliMetBD.sql

--- poliMetBD.sql
+++ poliMetBD.sql
@@ -16,13 +16,13 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `artistas`
+-- Table structure for table `artista`
 --
 
-DROP TABLE IF EXISTS `artistas`;
+DROP TABLE IF EXISTS `artista`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `artistas` (
+CREATE TABLE `artista` (
   `id_artista` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(100) DEFAULT NULL,
   `pais` varchar(100) DEFAULT NULL,
@@ -32,22 +32,22 @@ CREATE TABLE `artistas` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `artistas`
+-- Dumping data for table `artista`
 --
 
-LOCK TABLES `artistas` WRITE;
-/*!40000 ALTER TABLE `artistas` DISABLE KEYS */;
-/*!40000 ALTER TABLE `artistas` ENABLE KEYS */;
+LOCK TABLES `artista` WRITE;
+/*!40000 ALTER TABLE `artista` DISABLE KEYS */;
+/*!40000 ALTER TABLE `artista` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `eventos`
+-- Table structure for table `evento`
 --
 
-DROP TABLE IF EXISTS `eventos`;
+DROP TABLE IF EXISTS `evento`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `eventos` (
+CREATE TABLE `evento` (
   `id_evento` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(100) DEFAULT NULL,
   `descripcion` text,
@@ -59,22 +59,22 @@ CREATE TABLE `eventos` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `eventos`
+-- Dumping data for table `evento`
 --
 
-LOCK TABLES `eventos` WRITE;
-/*!40000 ALTER TABLE `eventos` DISABLE KEYS */;
-/*!40000 ALTER TABLE `eventos` ENABLE KEYS */;
+LOCK TABLES `evento` WRITE;
+/*!40000 ALTER TABLE `evento` DISABLE KEYS */;
+/*!40000 ALTER TABLE `evento` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `membresias`
+-- Table structure for table `membresia`
 --
 
-DROP TABLE IF EXISTS `membresias`;
+DROP TABLE IF EXISTS `membresia`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `membresias` (
+CREATE TABLE `membresia` (
   `id_membresia` int NOT NULL AUTO_INCREMENT,
   `tipo` varchar(50) DEFAULT NULL,
   `descripcion` text,
@@ -83,49 +83,49 @@ CREATE TABLE `membresias` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `membresias`
+-- Dumping data for table `membresia`
 --
 
-LOCK TABLES `membresias` WRITE;
-/*!40000 ALTER TABLE `membresias` DISABLE KEYS */;
-/*!40000 ALTER TABLE `membresias` ENABLE KEYS */;
+LOCK TABLES `membresia` WRITE;
+/*!40000 ALTER TABLE `membresia` DISABLE KEYS */;
+/*!40000 ALTER TABLE `membresia` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `notificaciones`
+-- Table structure for table `notificacion`
 --
 
-DROP TABLE IF EXISTS `notificaciones`;
+DROP TABLE IF EXISTS `notificacion`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `notificaciones` (
+CREATE TABLE `notificacion` (
   `id_notificacion` int NOT NULL AUTO_INCREMENT,
   `id_usuario` int DEFAULT NULL,
   `mensaje` text,
   `fecha_creacion` datetime DEFAULT NULL,
   PRIMARY KEY (`id_notificacion`),
   KEY `id_usuario` (`id_usuario`),
-  CONSTRAINT `notificaciones_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`)
+  CONSTRAINT `notificacion_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `notificaciones`
+-- Dumping data for table `notificacion`
 --
 
-LOCK TABLES `notificaciones` WRITE;
-/*!40000 ALTER TABLE `notificaciones` DISABLE KEYS */;
-/*!40000 ALTER TABLE `notificaciones` ENABLE KEYS */;
+LOCK TABLES `notificacion` WRITE;
+/*!40000 ALTER TABLE `notificacion` DISABLE KEYS */;
+/*!40000 ALTER TABLE `notificacion` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `obras`
+-- Table structure for table `obra`
 --
 
-DROP TABLE IF EXISTS `obras`;
+DROP TABLE IF EXISTS `obra`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `obras` (
+CREATE TABLE `obra` (
   `id_obra` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(100) DEFAULT NULL,
   `descripcion` text,
@@ -138,29 +138,29 @@ CREATE TABLE `obras` (
   KEY `id_artista` (`id_artista`),
   KEY `id_sala` (`id_sala`),
   KEY `id_tipo_obra` (`id_tipo_obra`),
-  CONSTRAINT `obras_ibfk_1` FOREIGN KEY (`id_artista`) REFERENCES `artistas` (`id_artista`),
-  CONSTRAINT `obras_ibfk_2` FOREIGN KEY (`id_sala`) REFERENCES `salas` (`id_sala`),
-  CONSTRAINT `obras_ibfk_3` FOREIGN KEY (`id_tipo_obra`) REFERENCES `tipos_obra` (`id_tipo_obra`)
+  CONSTRAINT `obra_ibfk_1` FOREIGN KEY (`id_artista`) REFERENCES `artista` (`id_artista`),
+  CONSTRAINT `obra_ibfk_2` FOREIGN KEY (`id_sala`) REFERENCES `sala` (`id_sala`),
+  CONSTRAINT `obra_ibfk_3` FOREIGN KEY (`id_tipo_obra`) REFERENCES `tipos_obra` (`id_tipo_obra`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `obras`
+-- Dumping data for table `obra`
 --
 
-LOCK TABLES `obras` WRITE;
-/*!40000 ALTER TABLE `obras` DISABLE KEYS */;
-/*!40000 ALTER TABLE `obras` ENABLE KEYS */;
+LOCK TABLES `obra` WRITE;
+/*!40000 ALTER TABLE `obra` DISABLE KEYS */;
+/*!40000 ALTER TABLE `obra` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `pisos`
+-- Table structure for table `piso`
 --
 
-DROP TABLE IF EXISTS `pisos`;
+DROP TABLE IF EXISTS `piso`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `pisos` (
+CREATE TABLE `piso` (
   `id_piso` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(100) DEFAULT NULL,
   `descripcion` text,
@@ -169,22 +169,22 @@ CREATE TABLE `pisos` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `pisos`
+-- Dumping data for table `piso`
 --
 
-LOCK TABLES `pisos` WRITE;
-/*!40000 ALTER TABLE `pisos` DISABLE KEYS */;
-/*!40000 ALTER TABLE `pisos` ENABLE KEYS */;
+LOCK TABLES `piso` WRITE;
+/*!40000 ALTER TABLE `piso` DISABLE KEYS */;
+/*!40000 ALTER TABLE `piso` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `reservas`
+-- Table structure for table `reserva`
 --
 
-DROP TABLE IF EXISTS `reservas`;
+DROP TABLE IF EXISTS `reserva`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `reservas` (
+CREATE TABLE `reserva` (
   `id_reserva` int NOT NULL AUTO_INCREMENT,
   `id_usuario` int DEFAULT NULL,
   `id_obra` int DEFAULT NULL,
@@ -195,72 +195,72 @@ CREATE TABLE `reservas` (
   PRIMARY KEY (`id_reserva`),
   KEY `id_usuario` (`id_usuario`),
   KEY `id_obra` (`id_obra`),
-  CONSTRAINT `reservas_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`),
-  CONSTRAINT `reservas_ibfk_2` FOREIGN KEY (`id_obra`) REFERENCES `obras` (`id_obra`)
+  CONSTRAINT `reserva_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`),
+  CONSTRAINT `reserva_ibfk_2` FOREIGN KEY (`id_obra`) REFERENCES `obra` (`id_obra`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `reservas`
+-- Dumping data for table `reserva`
 --
 
-LOCK TABLES `reservas` WRITE;
-/*!40000 ALTER TABLE `reservas` DISABLE KEYS */;
-/*!40000 ALTER TABLE `reservas` ENABLE KEYS */;
+LOCK TABLES `reserva` WRITE;
+/*!40000 ALTER TABLE `reserva` DISABLE KEYS */;
+/*!40000 ALTER TABLE `reserva` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `salas`
+-- Table structure for table `sala`
 --
 
-DROP TABLE IF EXISTS `salas`;
+DROP TABLE IF EXISTS `sala`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `salas` (
+CREATE TABLE `sala` (
   `id_sala` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(100) DEFAULT NULL,
   `descripcion` text,
   `id_piso` int DEFAULT NULL,
   PRIMARY KEY (`id_sala`),
   KEY `id_piso` (`id_piso`),
-  CONSTRAINT `salas_ibfk_1` FOREIGN KEY (`id_piso`) REFERENCES `pisos` (`id_piso`)
+  CONSTRAINT `sala_ibfk_1` FOREIGN KEY (`id_piso`) REFERENCES `piso` (`id_piso`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `salas`
+-- Dumping data for table `sala`
 --
 
-LOCK TABLES `salas` WRITE;
-/*!40000 ALTER TABLE `salas` DISABLE KEYS */;
-/*!40000 ALTER TABLE `salas` ENABLE KEYS */;
+LOCK TABLES `sala` WRITE;
+/*!40000 ALTER TABLE `sala` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sala` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `tickets`
+-- Table structure for table `ticket`
 --
 
-DROP TABLE IF EXISTS `tickets`;
+DROP TABLE IF EXISTS `ticket`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `tickets` (
+CREATE TABLE `ticket` (
   `id_ticket` int NOT NULL AUTO_INCREMENT,
   `id_reserva` int DEFAULT NULL,
   `fecha_funcion` date DEFAULT NULL,
   `estado` enum('activo','usado','vencido') DEFAULT NULL,
   PRIMARY KEY (`id_ticket`),
   KEY `id_reserva` (`id_reserva`),
-  CONSTRAINT `tickets_ibfk_1` FOREIGN KEY (`id_reserva`) REFERENCES `reservas` (`id_reserva`)
+  CONSTRAINT `ticket_ibfk_1` FOREIGN KEY (`id_reserva`) REFERENCES `reserva` (`id_reserva`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `tickets`
+-- Dumping data for table `ticket`
 --
 
-LOCK TABLES `tickets` WRITE;
-/*!40000 ALTER TABLE `tickets` DISABLE KEYS */;
-/*!40000 ALTER TABLE `tickets` ENABLE KEYS */;
+LOCK TABLES `ticket` WRITE;
+/*!40000 ALTER TABLE `ticket` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ticket` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -288,13 +288,13 @@ LOCK TABLES `tipos_obra` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `usuarios`
+-- Table structure for table `usuario`
 --
 
-DROP TABLE IF EXISTS `usuarios`;
+DROP TABLE IF EXISTS `usuario`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `usuarios` (
+CREATE TABLE `usuario` (
   `id_usuario` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(100) DEFAULT NULL,
   `email` varchar(100) DEFAULT NULL,
@@ -302,17 +302,17 @@ CREATE TABLE `usuarios` (
   PRIMARY KEY (`id_usuario`),
   UNIQUE KEY `email` (`email`),
   KEY `id_membresia` (`id_membresia`),
-  CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`id_membresia`) REFERENCES `membresias` (`id_membresia`)
+  CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`id_membresia`) REFERENCES `membresia` (`id_membresia`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `usuarios`
+-- Dumping data for table `usuario`
 --
 
-LOCK TABLES `usuarios` WRITE;
-/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -321,16 +321,14 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `usuarios_eventos`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `usuarios_eventos` (
   `id_usuario` int NOT NULL,
   `id_evento` int NOT NULL,
   PRIMARY KEY (`id_usuario`,`id_evento`),
   KEY `id_evento` (`id_evento`),
-  CONSTRAINT `usuarios_eventos_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`),
-  CONSTRAINT `usuarios_eventos_ibfk_2` FOREIGN KEY (`id_evento`) REFERENCES `eventos` (`id_evento`)
+  CONSTRAINT `usuarios_eventos_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`),
+  CONSTRAINT `usuarios_eventos_ibfk_2` FOREIGN KEY (`id_evento`) REFERENCES `evento` (`id_evento`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `usuarios_eventos`
@@ -342,13 +340,13 @@ LOCK TABLES `usuarios_eventos` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `valoraciones`
+-- Table structure for table `valoracion`
 --
 
-DROP TABLE IF EXISTS `valoraciones`;
+DROP TABLE IF EXISTS `valoracion`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `valoraciones` (
+CREATE TABLE `valoracion` (
   `id_valoracion` int NOT NULL AUTO_INCREMENT,
   `id_usuario` int DEFAULT NULL,
   `id_obra` int DEFAULT NULL,
@@ -358,29 +356,29 @@ CREATE TABLE `valoraciones` (
   PRIMARY KEY (`id_valoracion`),
   KEY `id_usuario` (`id_usuario`),
   KEY `id_obra` (`id_obra`),
-  CONSTRAINT `valoraciones_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`),
-  CONSTRAINT `valoraciones_ibfk_2` FOREIGN KEY (`id_obra`) REFERENCES `obras` (`id_obra`),
-  CONSTRAINT `valoraciones_chk_1` CHECK ((`puntaje` between 1 and 5))
+  CONSTRAINT `valoracion_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`),
+  CONSTRAINT `valoracion_ibfk_2` FOREIGN KEY (`id_obra`) REFERENCES `obra` (`id_obra`),
+  CONSTRAINT `valoracion_chk_1` CHECK ((`puntaje` between 1 and 5))
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `valoraciones`
+-- Dumping data for table `valoracion`
 --
 
-LOCK TABLES `valoraciones` WRITE;
-/*!40000 ALTER TABLE `valoraciones` DISABLE KEYS */;
-/*!40000 ALTER TABLE `valoraciones` ENABLE KEYS */;
+LOCK TABLES `valoracion` WRITE;
+/*!40000 ALTER TABLE `valoracion` DISABLE KEYS */;
+/*!40000 ALTER TABLE `valoracion` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `visitas`
+-- Table structure for table `visita`
 --
 
-DROP TABLE IF EXISTS `visitas`;
+DROP TABLE IF EXISTS `visita`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `visitas` (
+CREATE TABLE `visita` (
   `id_visita` int NOT NULL AUTO_INCREMENT,
   `id_usuario` int DEFAULT NULL,
   `id_obra` int DEFAULT NULL,
@@ -388,18 +386,18 @@ CREATE TABLE `visitas` (
   PRIMARY KEY (`id_visita`),
   KEY `id_usuario` (`id_usuario`),
   KEY `id_obra` (`id_obra`),
-  CONSTRAINT `visitas_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`),
-  CONSTRAINT `visitas_ibfk_2` FOREIGN KEY (`id_obra`) REFERENCES `obras` (`id_obra`)
+  CONSTRAINT `visita_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`),
+  CONSTRAINT `visita_ibfk_2` FOREIGN KEY (`id_obra`) REFERENCES `obra` (`id_obra`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `visitas`
+-- Dumping data for table `visita`
 --
 
-LOCK TABLES `visitas` WRITE;
-/*!40000 ALTER TABLE `visitas` DISABLE KEYS */;
-/*!40000 ALTER TABLE `visitas` ENABLE KEYS */;
+LOCK TABLES `visita` WRITE;
+/*!40000 ALTER TABLE `visita` DISABLE KEYS */;
+/*!40000 ALTER TABLE `visita` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
